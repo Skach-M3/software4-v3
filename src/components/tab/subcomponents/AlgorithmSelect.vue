02e9aee3dd96d4d9d23cc365cce6fb2de76d9dfd
rzzz-ssq <1814616195@qq.com>
2024-11-12 09:10:31 +0800

    加入MIFS

--- src/components/tab/subcomponents/AlgorithmSelect.vue
+++ src/components/tab/subcomponents/AlgorithmSelect.vue
@@ -75,9 +75,6 @@
             </el-form>
             <div class="buttonBox">
               <el-button round @click="backStep()">上一步</el-button>
-              <el-button round @click="resetForm('SF_DRMB_ref')"
-                >恢复默认</el-button
-              >
               <el-button
                 type="primary"
                 round
@@ -120,7 +117,59 @@
             >
           </div>
           <div class="paramBox"></div></el-tab-pane>
+
+          <el-tab-pane
+          label="MIFS"
+          name="MIFS"
+        >
+          <div class="titleBox">MIFS</div>
+          <div class="introBox">
+            <p>模型说明：</p>
+            <p>
+              MIFS算法利用特征间的互信息，逐步筛选重要特征，减少冗余提升模型效果。
+            </p>
+          </div>
+          <div class="paramBox">
+            <el-form
+              :model="MIFS_form"
+              class=""
+              label-position="top"
+              ref="MIFS_ref"
+            >
+              <el-form-item prop="threshold">
+                <template slot="label">
+                  <span class="paramTitle">互信息阈值</span>
+                  <el-popover placement="right" trigger="hover">
+                    <div>
+                      互信息阈值是指在特征选择过程中，通过设置互信息的最低值来筛选相关性高的特征
+                    </div>
+                    <el-icon
+                      slot="reference"
+                      class="el-icon-warning-outline"
+                    ></el-icon>
+                  </el-popover>
+                </template>
+                <el-input v-model.trim="MIFS_form.threshold"></el-input>
+                <span class="valueRange">(取值范围为 0 - 1 )</span>
+              </el-form-item>
+            </el-form>
+            <div class="buttonBox">
+              <el-button round @click="backStep()">上一步</el-button>
+              <el-button round @click="resetForm('SF_DRMB_ref')"
+                >恢复默认</el-button
+              >
+              <el-button
+                type="primary"
+                round
+                @click="submit('/runtime_bus/mifs_algorithm')"
+                >提交运算</el-button
+              >
+            </div>
+          </div>
+        </el-tab-pane>
       </el-tabs>
+      
+      
       <!-- <el-switch v-model="value1">
       </el-switch>
       <el-switch v-model="value2" active-color="#13ce66" inactive-color="#ff4949"
@@ -152,6 +201,9 @@ export default {
         K_OR: 0,
         K_and_PC: 0,
         K_and_SP: 0,
+      },
+      MIFS_form:{
+        threshold:0.05
       },
       res: "",
     };
@@ -186,6 +238,7 @@ export default {
         K_and_pc: Number(this.m_SF_DRMB.K_and_PC),
         K_and_sp: Number(this.m_SF_DRMB.K_and_SP),
         knowledge: this.m_caculate_known_features,
+        mifs_threshold: Number(this.MIFS_form.threshold)
       };
       postRequest(url, payload)
         .then((res) => {
